b3185da15497c0b6cfa955366a7398a8d779445d
mioann47 <mioann47@cs.ucy.ac.cy>
2017-05-07 00:14:16 +0300

    clinic

---
+++ ClinicWrapper.java
@@ -0,0 +1,24 @@
+package epl362rest;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+//JAX-RS supports an automatic mapping from JAXB annotated class to XML and
+//JSON
+//Isn't that cool?
+public class ClinicWrapper {
+
+	
+	private ArrayList<Clinic> cList;
+
+	public ArrayList<Clinic> getcList() {
+		return cList;
+	}
+
+	public void setcList(ArrayList<Clinic> cList) {
+		this.cList = cList;
+	}
+	
+	
+}
