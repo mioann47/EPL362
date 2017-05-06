186b06f8149a263f255f3f1c9b09f7359eeff0e8
cchrys06 <cchrys06@cs.ucy.ac.cy>
2017-05-07 00:14:07 +0300

    appointmet class and wrapper

---
+++ AppointmentWrapper.java
@@ -0,0 +1,24 @@
+package epl362rest;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+//JAX-RS supports an automatic mapping from JAXB annotated class to XML and
+//JSON
+//Isn't that cool?
+public class AppointmentWrapper {
+
+private ArrayList<Appointment> aList;
+
+public ArrayList<Appointment> getaList() {
+	return aList;
+}
+
+public void setaList(ArrayList<Appointment> aList) {
+	this.aList = aList;
+}
+
+	
+}
